534abb5396750ae0f7151b598cd0554fc000939f
Nob Yuasys <yuasys@gmail.com>
2022-12-22 18:31:19 +0900

    initial commit

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function about() {
+  return (
+    <div>
+      <h1 className='text-3xl text-center mt-6'>About page</h1>
+    </div>
+  )
+}
+
+export default about
---
+++ pages/new.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function NewPage() {
+  return <div className="text-3xl text-center mt-6">add one product</div>;
+}
+
+export default NewPage;
